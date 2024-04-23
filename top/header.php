c830884af17e105b804b11da9bd9fdcdb9ee70c0
tsurumutsumi <mutsumitsuru1@gmail.com>
2024-04-23 17:14:44 +0900

    a

---
+++ top/header.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta http-equiv="Content-Security-Policy" content="default-src * data: gap: content: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+
+<!-- js使う時用 -->
+<script src="../js/　　　.js"></script>
+
+<!-- css使う時用 -->
+<link rel="stylesheet" href="../css/　　　.css">
+
+<script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+</head>
+<?php require '../hamburger_menu.php'; ?>
+<body>
+
