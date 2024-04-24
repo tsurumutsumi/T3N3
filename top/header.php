4b05b1a77219bec5c55374fb1d8958ef2c0995b1
tsurumutsumi <mutsumitsuru1@gmail.com>
2024-04-24 12:20:17 +0900

    トップ作成

---
+++ top/header.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta http-equiv="Content-Security-Policy" content="default-src * data: gap: content: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
+
+<!-- <script src="../js/index.js"></script> -->
+
+<!-- <link rel="stylesheet" href="../css/index.css"> -->
+
+<script src="https://ajaxzip3.github.io/ajaxzip3.js" charset="UTF-8"></script>
+</head>
+
+<body>
+<br><br>
