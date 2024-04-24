78253a378b56d7f4f75402c9616ce22db1c4e3c7
tsurumutsumi <mutsumitsuru1@gmail.com>
2024-04-24 12:45:10 +0900

    a

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php require 'top/db_connect.php'; ?>
+<?php require 'top/header.php'; ?>
+
+<div class="slideshow">
+        <!-- imagesフォルダ内にある画像を表示  -->
+        <!-- ここに4行追加 -->
+            <img src="img/kikou.png">
+            <img src="img/teitetsu.png">
+            <img src="img/taiiku_boushi_tate.png">
+            <img src="img/undoukai_pyramid.png">
+    </div>
+    <script src="script/jquery-3.7.0.min.js"></script>
+    <!-- スライドショーで使うプラグイン「slick」のJavaScriptを読み込む -->
+    <script src="slick/slick.min.js"></script>
+    <script src="script/slideshow.js"></script>
+</div>
+
+
