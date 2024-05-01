a2a451e1b93996cb8a4899ed48757483920bfde1
tsurumutsumi <mutsumitsuru1@gmail.com>
2024-05-01 09:28:43 +0900

    home

--- top/header.php
+++ top/header.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <meta http-equiv="Content-Security-Policy" content="default-src * data: gap: content: https://ssl.gstatic.com; style-src * 'unsafe-inline'; script-src * 'unsafe-inline' 'unsafe-eval'">
 
-<!-- <script src="../js/index.js"></script> -->
+<script src="../js/slideshow.js"></script>
 
 <!-- <link rel="stylesheet" href="../css/index.css"> -->
 
