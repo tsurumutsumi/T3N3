a323552f25cc01151deaec307b066035076b7982
aya2na <139205376+aya2na@users.noreply.github.com>
2024-05-02 12:08:11 +0900

    ログイン画面背景色変更

--- rogin.php
+++ rogin.php
@@ -1,8 +1,10 @@
-<?php require 'db-connect.php'; ?>
+<?php require 'top/header.php'; ?>
+<?php require 'top/db-connect.php'; ?>
+<link rel="stylesheet" href="./css/login.css">
     <form action="rogin-output.php" method="post">
         <h1>ログイン</h1>
         メールアドレス<input type="text" name="login"><br>
         パスワード<input type="password" name="password"><br>
         <input type="submit" value="ログイン">
 </form>
-<?php require 'footer.php'; ?>
+<?php require 'top/footer.php'; ?>
