5d01cc4f24e357a1f05db89e524bc5c89beefbbb
aya2na <139205376+aya2na@users.noreply.github.com>
2024-05-02 12:35:54 +0900

    roginフォント変更

--- rogin.php
+++ rogin.php
@@ -3,8 +3,8 @@
 <link rel="stylesheet" href="./css/login.css">
     <form action="rogin-output.php" method="post">
         <h1>ログイン</h1>
-        メールアドレス<input type="text" name="login"><br>
-        パスワード<input type="password" name="password"><br>
+        <p>メールアドレス<br><input type="text" name="login"></p>
+        <p>パスワード<br><input type="password" name="password"></p>
         <input type="submit" value="ログイン">
 </form>
 <?php require 'top/footer.php'; ?>
