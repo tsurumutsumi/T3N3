f10e4a1eb253836c1927f6ff4a20d0e0d7089680
tsurumutsumi <mutsumitsuru1@gmail.com>
2024-05-09 09:37:09 +0900

    仮登録等

---
+++ sign_up/sign_up.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require '../top/db-connect.php';
+require '../top/header.php';
+
+
+
+require '../top/footer.php';
+?>
---
+++ sign_up/tentative_register.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require '../top/db-connect.php';
+require '../top/header.php';
+
+echo '<h1>かりとうろく</h1>';
+echo ''
+
+require '../top/footer.php';
+?>
