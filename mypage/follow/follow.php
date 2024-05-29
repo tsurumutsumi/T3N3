0711450c1ad69f58ad08d02608f4db860e3392db
takeyamaseito <2201156@s.asojuku.ac.jp>
2024-05-29 11:58:37 +0900

    a

--- mypage/follow/follow.php
+++ mypage/follow/follow.php
@@ -1,11 +1,11 @@
 <?php
-$current_user = get_user($_GET['user_id']);
-$profile_user = get_user($_GET['page_id']);
+//$current_user = get_user($_GET['user_id']);
+$user_management = get_user($_GET['user_id']);
 ?>
 <form action="#" method="post">
           <input type="hidden" class="current_user_id" value="<?= $current_user['id'] ?>">
           <input type="hidden" name="follow_user_id" value="<?= $user_id ?>">
-          <?php if (check_follow($current_user['id'],$user_id)): ?>
+          <?php if (check_follow($user_management['user_name'],$user_id)): ?>
           <button class="follow_btn border_white btn following" type="button" name="follow">フォロー中</button>
           <?php else: ?>
           <button class="follow_btn border_white btn" type="button" name="follow">フォロー</button>
