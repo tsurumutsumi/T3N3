c3346093afebd2eb03cc3093d72761e8b3142148
aya2na <139205376+aya2na@users.noreply.github.com>
2024-06-10 09:38:21 +0900

    カーソル時の動作js追加

---
+++ js/profile_change.js
@@ -0,0 +1,13 @@
+function changeText(button, isHovering) {
+    if (isHovering) {
+      // カーソルがボタンに乗った時
+      if (button.classList.contains('return_button')) {
+        button.textContent = "▶戻る";
+      } 
+    } else {
+      // カーソルがボタンから離れた時
+      if (button.classList.contains('return_button')) {
+        button.textContent = "戻る";
+      } 
+    }
+  }
