9ee2341a55302eb60e86ee5b8f323e835ca6c3bc
Sho-t4 <148170997+Sho-t4@users.noreply.github.com>
2024-07-09 12:38:10 +0900

    変更

--- mypage/custom_mypage.php
+++ mypage/custom_mypage.php
@@ -110,12 +110,14 @@ ob_end_flush(); // 出力バッファリングを終了
                                     <img src="<?php echo $imagePath; ?>" alt="投稿画像" class="post_img">
                                     <p class="post_comment"><?php echo htmlspecialchars($post['comment']); ?></p>
                                     <?
-                                     $likeButtonSrc = in_array($row['post_id'], $userLikes) ? 'img/mark_heart_red.png' : 'img/mark_heart_gray.png';
+                                     $likeButtonSrc = in_array($row['post_id'], $userLikes) ? '../img/mark_heart_red.png' : '../img/mark_heart_gray.png';
                                      echo '<input type="image" class="like-button" data-post-id="', htmlspecialchars($row['post_id'] ?? 0), '" src="', $likeButtonSrc, '" alt="いいね">';
                                      echo '<span class="like-count">', htmlspecialchars($row['like_count'] ?? 0), '</span>';
-                                     $followButtonSrc = in_array($row['user_id'], $userFollow) ? 'img/hito_blue.png' : 'img/hito_gray.png';
-                                     echo '<input type="image" src="', $followButtonSrc, '" class="follow-button" data-user-id="', htmlspecialchars($row['user_id']), '" alt="フォロー">';
-                                     ?>
+                                 
+                                     // フォローボタンを追加
+                                     $followButtonSrc = in_array($row['user_id'], $userFollow) ? '../img/hito_blue.png' : '../img/hito_gray.png';
+                                     echo '<input type="image" src="', $followButtonSrc, '" class="follow-button" data-user-id="', htmlspecialchars($row['user_id']), '" alt="フォロー" width=100px,height=100px>';
+                                    ?>
                                 </div>
                             </div>
                         </li>
